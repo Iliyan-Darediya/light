8e619b030e1c5eb72550f85e2416de3648417400
Iliyan-Darediya <63348176+Iliyan-Darediya@users.noreply.github.com>
2022-01-26 21:35:33 -0800

    Add setTitle to the button

--- light/light/ViewController.swift
+++ light/light/ViewController.swift
@@ -3,25 +3,29 @@
 //  light
 //
 //  Created by Iliyan Darediya on 26/01/22.
-//
+//Excerpt From: Apple Education. “Develop in Swift Fundamentals”. Apple Inc. - Education, 2021. Apple Books. https://books.apple.com/ca/book/develop-in-swift-fundamentals/id1556365994
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lightButton: UIButton!
     var lightOn = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
 
 
     fileprivate func updateUI() {
         if lightOn {
             view.backgroundColor = .white
+            lightButton.setTitle("Off", for: .normal)
+            
         } else {
             view.backgroundColor = .black
+            lightButton.setTitle("On", for: .normal)
         }
     }
     
